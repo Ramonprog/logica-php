c43a7de3afd1031623c8e8dd8aa65be1ebb05a40
Ramonprog <programador.ramon@gmail.com>
2024-11-23 14:49:26 -0300

    datatypes

---
+++ dataTypes.php
@@ -0,0 +1,16 @@
+<?php
+
+$isStudent = true;
+$age = 25;
+
+var_dump($isStudent, true, $age, $age > 18);
+
+$score = [86, "90",95.5];
+
+var_dump($score[0] + $score[1] + $score[2]);
+
+//convertendo para string
+
+$a = "55";
+
+var_dump((int)$a);
