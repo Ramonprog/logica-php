b6d843e196d61c67103a340302118d342456f442
Ramonprog <programador.ramon@gmail.com>
2024-11-23 19:22:04 -0300

    require/include

---
+++ require/main.php
@@ -0,0 +1,10 @@
+<?php
+
+include_once "conf.php";
+
+echo "Database host: $dbHost \n";
+echo "Database user: $dbUser \n";
+
+// Use require quando o arquivo for indispensável para o funcionamento do sistema
+
+// Use include quando o arquivo for opcional ou secundário
